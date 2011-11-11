01c7592df586f94323550a8cc81c6c7baedc8844
unknown <jasonch@jasonch-PC.(none)>
2011-11-10 21:51:17 -0800

    Fixed some bugs =/

--- filterSIFTs.m
+++ filterSIFTs.m
@@ -1,4 +1,4 @@
-function filteredSift = filterSIFT()
+function filteredSift = filterSIFTs()
 
   synset = 'n04398044';
 
@@ -6,20 +6,21 @@ function filteredSift = filterSIFT()
   filteredSift = filteredSift.image_vldsift;
   siftImageIDs = IDstructToVector(filteredSift, synset);
 
-  %{ cleamImageIDs = %} load('cleanImages.mat'); 
+  % cleamImageIDs = %
+    load('cleanImages.mat'); 
 
   for i = 1:size(cleanImageIDs,1)
-    siftIndex = find(siftImageIDs == cleamImageIDs(i));
-    curSIFT = filteredSift(siftIndex).vldsift; 
+    siftIndex = find(siftImageIDs == cleanImageIDs(i));
 
-    %{ segLabels = %} load([cleanImageIDs(i) '_segment.mat']);
+    % segLabels = %
+      load([num2str(cleanImageIDs(i)) '_segment.mat']);
     
     % convert to pixel locations on segLabels
     width = size(segLabels,2);
     height = size(segLabels,1);
     locations = [round(filteredSift(siftIndex).x * width); round(filteredSift(siftIndex).y*height)]';
     for j = 1:size(locations,1)
-      if (segLabels(locations(j, 1), locations(j, 1)) == 0)
+      if (segLabels(locations(j, 1), locations(j, 2)) == 0)
         % if seglabel is 0, remove the element from sift feature set
         filteredSift(siftIndex).vldsift.x(j) = [];
         filteredSift(siftIndex).vldsift.y(j) = [];
@@ -35,11 +36,12 @@ function filteredSift = filterSIFT()
 end
 
 function ids =  IDstructToVector(sift, synset)
-  ids = [];
+  ids = zeros(size(sift,1),1);
   for i = 1:size(sift,1)
     % converts image IDs of the form 'synset_imageId' to numerical imageId
     % and store in the returning vector
-    ids = [ids; str2num(strrep(sift(i).ID, [synset '_'], ''))];
+    % note using str2double for speed, and rouding to get integer values
+    ids(i) = str2double(strrep(sift(i).ID, [synset '_'], ''));
   end
 end
 
