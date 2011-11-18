adc5989a4a111c513fe92219c7b39d88dff101e9
bayanbatn <bayanbatn@gmail.com>
2011-11-18 00:44:42 -0800

    added BoW histogram method

---
+++ computeBoWHistogram.m
@@ -0,0 +1,29 @@
+function bow_histogram = computeBoWHistogram( sift_descriptors, bow_dictionary )
+
+%given descriptors of an image, return the bow 
+
+[hist_len, ~] = size(bow_dictionary);
+
+bow_histogram = zeros(1, hist_len);
+sift_comp_dists = zeros(1, hist_len);
+
+for i=1:hist_len
+    sift_desc = sift_descriptors(i,:);
+    
+    for j=1:hist_len
+        word_mean = bow_dictionary(j,:);
+        sift_comp_dists(j) = norm(word_mean - sift_desc, 2);
+    end
+    
+    [~, min_idx] = min(sift_comp_dists);
+    
+    bow_histogram(min_idx) = bow_histogram(min_idx) + 1; 
+end
+
+end
+
+
+
+%1) find multiple synsets to train on 
+%2) for each image in a synset, get SIFT features matrix 
+
