b05c3b08468194ee3b1b8c210f4565f9e3b46711
unknown <jasonch@jasonch-PC.(none)>
2011-11-18 00:50:28 -0800

    this computes the vocabulary set from all SIFT features in the data set

---
+++ computeVocabularySet.m
@@ -0,0 +1,17 @@
+% this function accepts SIFTs as a Nx128 matrix, where each row is a SIFT feature
+% and uses K-means to compute K-clusters on the matrix
+% returns the centroids of each cluster in the Kx128 result
+
+function vocabulary = computeVocabularySet(sifts,k)
+  siftMatrix = convertSIFT2Matrix(sifts);
+  [IDX, vocabulary] = kmeans(siftMatrix, k);
+  vocabulary = vocabulary';
+end
+
+
+function siftMatrix = convertSIFT2Matrix(siftsObj)
+  siftMatrix = [];
+  for i=1:size(siftObj,1)
+    siftMatrix = [siftMatrix; siftObj(i).vldsift.desc'];
+  end
+end
