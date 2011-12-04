6a4b4714b1175e6f3c64ff12357b07d7dc8f47de
Chun-Chang Chen <jasonch@stanford.edu>
2011-12-04 04:54:03 -0800

    make labels and samples in sync

--- randomizeTrainingData.m
+++ randomizeTrainingData.m
@@ -2,7 +2,8 @@
 function [permuted_training_data, permuted_label] = randomizeTrainingData(training_data, training_label)
 
 %Label entries correspond to a row of data entries
-permuted_training_data = training_data(randperm(size(training_data,1)),:);
-permuted_label = training_label(randperm(size(training_label,1)),:);
+permute = randperm(size(training_data,1));
+permuted_training_data = training_data(permute,:);
+permuted_label = training_label(permute,:);
 
 end
